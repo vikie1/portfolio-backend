5e8e8dc2be666cbcba8835422c04fd8b9fc54988
vikie1 <39300268+vikie1@users.noreply.github.com>
2021-09-27 14:21:22 +0300

    UI additions for the documentation

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,8 @@
-import {home} from "/modules/home.js"
+import {home} from "/modules/home.jsx"
 const routes = {
 '/': home
 }
 const render = () => {
-    document.getElementById("content").innerHTML = home();
+    document.getElementById("content").innerHTML = document.getElementById('stupid');
 }
 window.onload=render();
