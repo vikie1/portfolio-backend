3fbe67aa0594455e9125a37c1b36994c03427cbc
vikie1 <39300268+vikie1@users.noreply.github.com>
2021-09-28 15:19:59 +0300

    few modifications to the ui

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,14 @@
-import {home} from "/modules/home.jsx"
-const routes = {
-'/': home
-}
-const render = () => {
-    document.getElementById("content").innerHTML = document.getElementById('stupid');
-}
-window.onload=render();
+function getDom(element){
+    fetch('/components/html/unfinished.html')
+    .then((response) => response.text())
+    .then((html) => {
+        element.innerHTML = html;
+    })
+    .catch((error) => {
+        element.innerHTML = "action not complete due to " + error; 
+    })
+};
+
+document.getElementById("start").addEventListener("click", function () {
+  getDom(document.body)
+});
