c34d321539188cea2cbf032e61a457d30028c38c
vikie1 <39300268+vikie1@users.noreply.github.com>
2021-12-02 13:07:31 +0300

    switched from application.properties to application.yml

--- src/main/java/io/github/vikie1/portfolio/PortfolioApplication.java
+++ src/main/java/io/github/vikie1/portfolio/PortfolioApplication.java
@@ -2,6 +2,8 @@ package io.github.vikie1.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class PortfolioApplication {
