59569d165849c34228e05b8a870f8526059fd5a2
Victor Mwangi <39300268+vikie1@users.noreply.github.com>
2023-11-20 17:51:42 +0300

    update swagger documentation library

--- src/main/java/io/github/vikie1/portfolio/configuration/SwaggerConf.java
+++ src/main/java/io/github/vikie1/portfolio/configuration/SwaggerConf.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
